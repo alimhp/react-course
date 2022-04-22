add52df0e01a41b872cbcec887fc94c58b21472d
alimhp <alimohamadipourr@gmail.com>
2022-04-22 06:18:57 -0700

    24 inc

--- src/component/product/ProductList.js
+++ src/component/product/ProductList.js
@@ -4,9 +4,9 @@ import Product from "./product";
 class ProductList extends Component {
   state = {
     product: [
-      { title: "react.js", price: "99$", id: 1 },
-      { title: "java.js", price: "89$", id: 2 },
-      { title: "pay.js", price: "79$", id: 3 },
+      { title: "react.js", price: "99$", id: 1, quantity: 1 },
+      { title: "java.js", price: "89$", id: 2, quantity: 2 },
+      { title: "pay.js", price: "79$", id: 3, quantity: 3 },
     ],
   };
   //  for removing item
@@ -20,8 +20,12 @@ class ProductList extends Component {
         {this.state.product.map((product, index) => {
           return (
             <Product
-              name={product.title}
-              price={product.price}
+              // name={product.title}
+              // price={product.price}
+              // quantity={product.quantity}
+
+              // use product(props) to have more space
+              product={product}
               key={index}
               // for removing item
               onDelet={() => this.removeHandler(product.id)}
--- src/component/product/product.js
+++ src/component/product/product.js
@@ -1,11 +1,19 @@
-import style from "./product.module.css";
+import styles from "./product.module.css";
 const Product = (props) => {
   return (
-    <div className={style.product} onClick={props.click}>
-      <p>product name :{props.name}course</p>
-      <p>procut price:{props.price} </p>
+    <div className={styles.product} onClick={props.click}>
+      <p>product name :{props.product.name}course</p>
+      <p>procut price:{props.product.price} </p>
+
+      <span className={styles.value}>{props.product.quantity}</span>
+
+      <button className={`${styles.button} ${styles.inc}`}>increment</button>
+      {/* <button clasname={`${style.button} ${style.inc}`}>increment</button> */}
+
       {/* for removing item */}
-      <button onClick={props.onDelet}> delet </button>
+      <button className={styles.button} onClick={props.onDelet}>
+        delet
+      </button>
     </div>
   );
 };
