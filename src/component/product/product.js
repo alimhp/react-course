10838409d5b2c55360350cc9cd1783ab941284e1
alimhp <alimohamadipourr@gmail.com>
2022-04-28 06:16:50 -0700

    lifting state&object destrcting

--- src/component/product/product.js
+++ src/component/product/product.js
@@ -1,34 +1,33 @@
 import styles from "./product.module.css";
 import { BiTrash } from "react-icons/bi";
 
-const Product = (props) => {
+const Product = ({ product, onChange, onIncrement, onDicrement, onDelet }) => {
   return (
-    <div className={styles.product} onClick={props.click}>
-      <p>product name :{props.product.title}course</p>
-      <p>procut price:{props.product.price} </p>
+    <div className={styles.product}>
+      <p>product name :{product.title}course</p>
+      <p>procut price:{product.price} </p>
 
-      <span className={styles.value}>{props.product.quantity}</span>
-      <input onChange={props.onChange} value={props.product.title} />
-      {/* controled component ==> value={props.product.title} */}
+      <span className={styles.value}>{product.quantity}</span>
+      <input onChange={onChange} value={product.title} />
+      {/* controled component ==> value={product.title} */}
       <button
-        onClick={props.onIncrement}
+        onClick={onIncrement}
         className={`${styles.button} ${styles.inc}`}
       >
         +
       </button>
       <button
-        onClick={props.onDicrement}
+        onClick={onDicrement}
         className={`${styles.button} 
-          ${props.product.quantity === 1 ? styles.remove : null}`}
+          ${product.quantity === 1 ? styles.remove : null}`}
         // or we can do this
-        // ${props.product.quantity === 1 && styles.remove}`
-
+        // ${product.quantity === 1 && styles.remove}`
       >
-        {props.product.quantity > 1 ? "-" : <BiTrash />}
+        {product.quantity > 1 ? "-" : <BiTrash />}
       </button>
 
       {/* for removing item */}
-      <button className={styles.button} onClick={props.onDelet}>
+      <button className={styles.button} onClick={onDelet}>
         delet
       </button>
     </div>
